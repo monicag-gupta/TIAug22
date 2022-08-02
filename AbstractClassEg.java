b16d6f0055336b15013d073f0dc28d7665dc24e2
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2022-08-02 17:54:18 +0530

    Create AbstractClassEg.java

---
+++ AbstractClassEg.java
@@ -0,0 +1,34 @@
+abstract class OS{
+	public abstract void cmdLine(); 
+	public void dir(){
+		System.out.println("Dir/folder System");
+	}
+	public void FS(){
+		System.out.println("File System");
+	}
+	
+}
+class WinOS extends OS{
+	public void cmdLine(){
+		System.out.println("Cmd");
+	}
+}
+class MacOS extends OS{
+	public void cmdLine(){
+		System.out.println("Terminal");
+	}
+}
+public class AbstractClassEg {
+	public void workingOS(OS os){
+		os.cmdLine();
+		os.dir();
+		os.FS();
+	}
+
+	public static void main(String[] args) {
+		
+		new AbstractClassEg().workingOS(new WinOS());
+
+	}
+
+}
